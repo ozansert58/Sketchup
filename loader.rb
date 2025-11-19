b3ff30a47cfb64f3698b60b3dd680aebdfa3d0ac
ozansert58 <155767229+ozansert58@users.noreply.github.com>
2025-11-19 21:34:16 +0300

    Create loader.rb file with SketchUp plugin loader code

---
+++ loader.rb
@@ -0,0 +1,12 @@
+# SketchUp Plugin Loader
+
+# This loader initializes the SketchUp plugin.
+
+module MySketchUpPlugin
+  def self.load
+    # Load necessary files and configurations here
+    puts 'Loading My SketchUp Plugin...'
+  end
+end
+
+MySketchUpPlugin.load
